383cb511cb798f78bbbaf204e4cfd79e41b865c4
Mayamohamed207 <mayamohamed364@gmail.com>
2025-11-30 15:49:25 +0200

    live camera added

--- lib/screens/pet_feeder_screen.dart
+++ lib/screens/pet_feeder_screen.dart
@@ -22,7 +22,7 @@ class _PetFeederScreenState extends State<PetFeederScreen> {
   }
 
   Future<void> _launchCameraStream() async {
-    const String espCameraUrl = 'http://10.123.28.89:81/stream';
+    const String espCameraUrl = 'https://unsunny-botchiest-khloe.ngrok-free.dev/stream';
     
     final uri = Uri.parse(espCameraUrl);
     if (await canLaunchUrl(uri)) {
